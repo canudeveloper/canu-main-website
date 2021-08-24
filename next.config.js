976d57df98378922c6cddeb3cced315208200931
Michael Vasallo <mjrvasallo@gmail.com>
2021-08-24 00:52:00 -0500

    Upgrade to Next.js 11

--- next.config.js
+++ next.config.js
@@ -4,12 +4,4 @@ module.exports = {
   images: {
     domains: ['localhost'],
   },
-  webpack: (config, { isServer }) => {
-    // Fixes packages that depend on fs/module module
-    if (!isServer) {
-      config.node = { fs: 'empty', module: 'empty' }
-    }
-
-    return config
-  },
 }

3413c8e1749c008d0e693d09baa5455f8a05d4b2
Michael Vasallo <mjrvasallo@gmail.com>
2021-08-24 01:17:10 -0500

    Add hover state to button

--- components/elements/Button.js
+++ components/elements/Button.js
@@ -2,7 +2,7 @@ import tw from 'twin.macro'
 
 const Primary = tw.button`px-2.5 py-1.5 rounded font-bold uppercase text-sm-none tracking-button bg-primary text-white hocus:bg-primary-light`
 const Secondary = tw.button`px-2.5 py-1.5 rounded font-bold uppercase text-sm-none tracking-button bg-secondary text-white hocus:bg-secondary-light`
-const Link = tw.a`inline-block font-bold text-primary underline`
+const Link = tw.a`inline-block font-bold text-primary hocus:text-primary-light underline`
 
 const buttons = {
   Primary,

726e63bed6e3c266b95e91c2145ffc52d1d19117
Michael Vasallo <mjrvasallo@gmail.com>
2021-10-20 22:41:43 -0500

    Fix warnings for button component

--- components/elements/Button.js
+++ components/elements/Button.js
@@ -1,18 +1,29 @@
+import React from 'react'
 import tw from 'twin.macro'
 
 const Primary = tw.button`px-2.5 py-1.5 rounded font-bold uppercase text-sm-none tracking-button bg-primary text-white hocus:bg-primary-light`
 const Secondary = tw.button`px-2.5 py-1.5 rounded font-bold uppercase text-sm-none tracking-button bg-secondary text-white hocus:bg-secondary-light`
 const Link = tw.a`inline-block font-bold text-primary hocus:text-primary-light underline`
 
-const buttons = {
+const styles = {
   Primary,
   Secondary,
   Link,
 }
 
-export default function Button({ variant = 'link', children, ...props }) {
-  const Component = buttons[variant.charAt(0).toUpperCase() + variant.slice(1)]
+const Button = React.forwardRef(
+  ({ variant = 'link', children, ...props }, ref) => {
+    const Component = styles[variant.charAt(0).toUpperCase() + variant.slice(1)]
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...props}>{children}</Component>
-}
+    return (
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      <Component ref={ref} {...props}>
+        {children}
+      </Component>
+    )
+  }
+)
+
+Button.displayName = 'Button'
+
+export default Button
